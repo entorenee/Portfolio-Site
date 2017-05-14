93a00609dfe65a74984fe0a6eb7c3434d6bb1d46
Daniel Lemay <d.s.lemay@icloud.com>
2017-05-14 08:49:39 -0700

    Initial commit of basic site

---
+++ assets/js/functions.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+  smoothScroll(300);
+});
+
+
+function smoothScroll (duration) {
+	$('a[href^="#"]').on('click', function(event) {
+
+	    var target = $( $(this).attr('href') );
+
+	    if( target.length ) {
+	        event.preventDefault();
+	        $('html, body').animate({
+	            scrollTop: target.offset(-75).top
+	        }, duration);
+	    }
+	});
