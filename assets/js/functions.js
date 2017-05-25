96f2f6740761198ba8f64f851cacd6d8082a74f8
Daniel Lemay <d.s.lemay@icloud.com>
2017-05-24 22:19:15 -0700

    Add modal dialog functions to script file
    Javascript functionality has been added to display and hide the modal
    dialog for the portfolio section. The closeModal function waits for the
    user to click the x in the dialog and will then hide it.
    
    The openModal function takes the id attribute of the selector to save
    as a variable and parse out the elements for the dialog box from a JSON
    file. This will allow for better performance by only loading the larger
    images when a user clicks on the accompanying project.

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
   smoothScroll(300);
   navbarToggle();
+  closeModal();
+  openModal();
 });
 
 
@@ -21,5 +23,27 @@ function smoothScroll (duration) {
 function navbarToggle() {
   $('.nav-link').on('click', function() {
     $('.navbar-toggler').click(); //Closes navbar before scrolling to section.
+  });
+}
+
+//Functions for showing and hiding the modal dialog box in portfolio section
+
+function closeModal() {
+  $('.close').on('click', function() {
+    $('.modal').css('display','none');
+  });
+}
+
+function openModal() {
+  $('div.portfolio-thumb').children().on('click', function(e) {
+    e.preventDefault();
+    var project = $(this).attr('id');
+    $.getJSON('/assets/js/projects.json', function(data) {
+      $('#project-name').html(data[project].title);
+      $('#project-large-thumb').attr('src', data[project].thumbSource);
+      $('#project-description').html(data[project].description);
+      $('#project-link').attr('href', data[project].projectLink);
+      $('.modal').css('display','block');
+    });
   });
 }
