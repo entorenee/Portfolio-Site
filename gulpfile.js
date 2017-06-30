f2b541c9b0b04dd583a607e40f855df2d02c0b96
Daniel Lemay <d.s.lemay@icloud.com>
2017-06-29 20:54:13 -0700

    Add imagemin module to Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,15 @@
 const gulp = require('gulp');
 const changed = require('gulp-changed');
+const imagemin = require('gulp-imagemin');
 
 gulp.task('copy', function(){
   return gulp.src(['!../projects/**/prepros-6.config','../projects/**/*'])
              .pipe(changed('./projects/'))
              .pipe(gulp.dest('./projects/'));
+});
+
+gulp.task('imagemin', function(){
+  gulp.src('./assets/img/twitch-viewer/src/*')
+      .pipe(imagemin())
+      .pipe(gulp.dest('./assets/img/twitch-viewer/dist'))
 });
