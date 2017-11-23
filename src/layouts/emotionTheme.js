a172f0ccec0754e2d914c8647f514a9b74d62b5f
Daniel Lemay <d.s.lemay@icloud.com>
2017-11-23 14:15:51 -0800

    Add Emotion ThemeProvider

---
+++ src/layouts/emotionTheme.js
@@ -0,0 +1,8 @@
+const theme = {
+  baseColor: '#2D3B7F',
+  lightAccent: '#A6B6FF',
+  mediumAccent: '#5A77FF',
+  darkAccent: '#485FCC'
+};
+
+export default theme;
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,22 +1,24 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Link from 'gatsby-link'
-import Helmet from 'react-helmet'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Link from 'gatsby-link';
+import Helmet from 'react-helmet';
+import { ThemeProvider } from 'emotion-theming';
 
-import './index.css'
+import './index.css';
+import theme from './emotionTheme';
 
 const Header = () => (
   <div
     style={{
       background: 'rebeccapurple',
-      marginBottom: '1.45rem',
+      marginBottom: '1.45rem'
     }}
   >
     <div
       style={{
         margin: '0 auto',
         maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
+        padding: '1.45rem 1.0875rem'
       }}
     >
       <h1 style={{ margin: 0 }}>
@@ -24,7 +26,7 @@ const Header = () => (
           to="/"
           style={{
             color: 'white',
-            textDecoration: 'none',
+            textDecoration: 'none'
           }}
         >
           Gatsby
@@ -32,33 +34,35 @@ const Header = () => (
       </h1>
     </div>
   </div>
-)
+);
 
 const TemplateWrapper = ({ children }) => (
-  <div>
-    <Helmet
-      title="Gatsby Default Starter"
-      meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
-      ]}
-    />
-    <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
+  <ThemeProvider theme={theme}>
+    <div>
+      <Helmet
+        title="Gatsby Default Starter"
+        meta={[
+          { name: 'description', content: 'Sample' },
+          { name: 'keywords', content: 'sample, something' }
+        ]}
+      />
+      <Header />
+      <div
+        style={{
+          margin: '0 auto',
+          maxWidth: 960,
+          padding: '0px 1.0875rem 1.45rem',
+          paddingTop: 0
+        }}
+      >
+        {children()}
+      </div>
     </div>
-  </div>
-)
+  </ThemeProvider>
+);
 
 TemplateWrapper.propTypes = {
-  children: PropTypes.func,
-}
+  children: PropTypes.func
+};
 
-export default TemplateWrapper
+export default TemplateWrapper;
