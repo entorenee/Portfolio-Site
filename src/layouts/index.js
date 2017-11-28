1e3af87d03d4d5e0df7a5bdaf7aec0bdedc0bd6b
Daniel Lemay <d.s.lemay@icloud.com>
2017-11-27 20:21:37 -0800

    Remove styling from index layout

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -5,7 +5,7 @@ import Helmet from 'react-helmet';
 import { ThemeProvider } from 'emotion-theming';
 import Header from '../components/header/Header';
 
-import './index.css';
+// import './index.css';
 import theme from './emotionTheme';
 
 const TemplateWrapper = ({ children }) => (
@@ -19,16 +19,7 @@ const TemplateWrapper = ({ children }) => (
         ]}
       />
       <Header />
-      <div
-        style={{
-          margin: '0 auto',
-          maxWidth: 960,
-          padding: '0px 1.0875rem 1.45rem',
-          paddingTop: 0
-        }}
-      >
-        {children()}
-      </div>
+      <div>{children()}</div>
     </div>
   </ThemeProvider>
 );
