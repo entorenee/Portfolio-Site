1bf400662cc56edf59b748d5ff2bf884ae65c136
Daniel Lemay <d.s.lemay@icloud.com>
2017-11-28 21:26:41 -0800

    Import About component and use fragments

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Link from 'gatsby-link';
+import About from '../components/About';
+
+const { Fragment } = React;
 
 const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>
+  <Fragment>
+    <About />
+  </Fragment>
 );
 
 export default IndexPage;
