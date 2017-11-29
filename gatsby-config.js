6993a3ba7999bba40f5b746e957b8e95be59cc74
Daniel Lemay <d.s.lemay@icloud.com>
2017-11-28 21:26:19 -0800

    Add gatsby-plugin-react-next

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,10 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Gatsby Default Starter`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-emotion`
-  ],
-}
+    `gatsby-plugin-emotion`,
+    `gatsby-plugin-react-next`
+  ]
+};
