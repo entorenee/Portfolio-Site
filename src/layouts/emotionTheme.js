8b8be1715b3371132cbda5072bed1de166600e06
Daniel Lemay <d.s.lemay@icloud.com>
2017-11-28 17:18:48 -0800

    Add margins object to theme

--- src/layouts/emotionTheme.js
+++ src/layouts/emotionTheme.js
@@ -1,8 +1,18 @@
+import { css } from 'emotion';
+
 const theme = {
   baseColor: '#2D3B7F',
   lightAccent: '#A6B6FF',
   mediumAccent: '#5A77FF',
-  darkAccent: '#485FCC'
+  darkAccent: '#485FCC',
+  margins: css`
+    width: 80%;
+    margin: 0 auto;
+
+    @media (max-width: 450px) {
+      width: 90%;
+    }
+  `
 };
 
 export default theme;
