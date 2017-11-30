cf72f9ea80b9a75b5007b9f3d536b148c331a68b
Daniel Lemay <d.s.lemay@icloud.com>
2017-11-29 22:21:42 -0800

    Create and add work section component

---
+++ src/components/work/Work.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import Carousel from './Carousel';
+
+const Work = () => <Carousel />;
+
+export default Work;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Link from 'gatsby-link';
 import About from '../components/about/About';
+import Work from '../components/work/Work';
 
 const { Fragment } = React;
 
 const IndexPage = () => (
   <Fragment>
     <About />
+    <Work />
   </Fragment>
 );
 
