52caf15f6c6e93d92e7e8484b083dc766a932abf
Daniel Lemay <d.s.lemay@icloud.com>
2017-12-03 13:36:35 -0800

    Create initial tests for Carousel component
    Created tests to check default state and the contents of the
    projectSpotlight file being imported.

---
+++ __tests__/Carousel.test.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import Carousel from '../src/components/work/Carousel';
+import projectSpotlight from '../src/components/work/projectSpotlight';
+import { mount } from 'enzyme';
+
+describe('Carousel', () => {
+  let mountedCarousel;
+
+  const carousel = () => {
+    if (!mountedCarousel) {
+      mountedCarousel = mount(<Carousel />);
+    }
+    return mountedCarousel;
+  };
+
+  beforeEach(() => {
+    mountedCarousel = undefined;
+  });
+
+  it('should have a default `currIndex` state of 0', () => {
+    expect(carousel().state().currIndex).toBe(0);
+  });
+
+  it('should have a default `isPlaying` state of false', () => {
+    expect(carousel().state().isPlaying).toBe(false);
+  });
+});
+
+describe('projectSpotlight', () => {
+  it('should be an array', () => {
+    expect(Array.isArray(projectSpotlight)).toBe(true);
+  });
+
+  it('should have at least one item', () => {
+    expect(projectSpotlight.length).toBeGreaterThan(0);
+  });
+
+  it('should be an array of objects', () => {
+    projectSpotlight.map(project => {
+      expect(typeof project).toBe('object');
+    });
+  });
+
+  it('should contain a defined title key', () => {
+    projectSpotlight.map(project => {
+      expect(project.hasOwnProperty('title')).toBe(true);
+      expect(project.title).toBeTruthy();
+    });
+  });
+
+  it('should contain a defined image key', () => {
+    projectSpotlight.map(project => {
+      expect(project.hasOwnProperty('image')).toBe(true);
+      expect(project.image).toBeTruthy();
+    });
+  });
+
+  it('should contain a defined projectLink key', () => {
+    projectSpotlight.map(project => {
+      expect(project.hasOwnProperty('projectLink')).toBe(true);
+      expect(project.projectLink).toBeTruthy();
+    });
+  });
+
+  it('should contain a defined githubLink key', () => {
+    projectSpotlight.map(project => {
+      expect(project.hasOwnProperty('githubLink')).toBe(true);
+      expect(project.githubLink).toBeTruthy();
+    });
+  });
+  it('should contain a defined description key', () => {
+    projectSpotlight.map(project => {
+      expect(project.hasOwnProperty('description')).toBe(true);
+      expect(project.description).toBeTruthy();
+    });
+  });
+});
