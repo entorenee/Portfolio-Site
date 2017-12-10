959c7918944c889f0d53f0518a3b46cb690aada3
Daniel Lemay <d.s.lemay@icloud.com>
2017-12-10 14:25:02 -0800

    Test MobileNavigation component

---
+++ __tests__/MobileNavigation.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import MobileNavigation from '../src/components/header/MobileNavigation';
+import { MemoryRouter } from 'react-router';
+import renderer from 'react-test-renderer';
+import { mount } from 'enzyme';
+
+describe('MobileNavigation', () => {
+  it('renders properly', () => {
+    const tree = renderer
+      .create(
+        <MemoryRouter>
+          <MobileNavigation />
+        </MemoryRouter>
+      )
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('toggleOpen method toggles isOpen boolean state', () => {
+    const wrapper = mount(<MobileNavigation />);
+    wrapper.instance().toggleOpen();
+    expect(wrapper.state().isOpen).toBe(true);
+    wrapper.instance().toggleOpen();
+    expect(wrapper.state().isOpen).toBe(false);
+  });
+});
