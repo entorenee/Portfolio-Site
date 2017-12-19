c3cb5bdf9fee5b46e1f15b96eca6cffd2497be09
Daniel Lemay <d.s.lemay@icloud.com>
2017-12-18 17:47:39 -0800

    Add tests for Button component

---
+++ __tests__/Button.test.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Button from '../src/components/Button';
+import { mount } from 'enzyme';
+import renderer from 'react-test-renderer';
+
+describe('Button', () => {
+  it('should render an anchor element if passed an href property', () => {
+    const wrapper = mount(<Button href="http://dslemay.com" />);
+    expect(wrapper.find('a').length).toBe(1);
+  });
+
+  it('renders properly when passed an href property', () => {
+    const tree = renderer.create(<Button href="http://dslemay.com" />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should render an anchor element if passed a to property', () => {
+    const wrapper = mount(<Button to="http://dslemay.com" />);
+    expect(wrapper.find('MockedLink').length).toBe(1);
+  });
+
+  it('renders properly when passed a to property', () => {
+    const tree = renderer.create(<Button to="http://dslemay.com" />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should render a basic button if not passed any props', () => {
+    const wrapper = mount(<Button />);
+    expect(wrapper.find('button').length).toBe(1);
+  });
+});
