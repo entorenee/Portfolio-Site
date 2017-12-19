96ad701abd177566198903a981897da5feb17594
Daniel Lemay <d.s.lemay@icloud.com>
2017-12-18 17:48:03 -0800

    Create button component
    The button component currently accepts three props. If it is passed an
    `href` prop it will render out an anchor element link. If it is passed
    a `to` prop it will render out a React link. If it is passed a button
    `type` it will change the type of button. Currently, the button can
    only have the types of 'button' and 'submit'.

---
+++ src/components/Button.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Link from 'gatsby-link';
+import styled from 'react-emotion';
+import { css } from 'emotion';
+
+const ButtonStyles = props => css`
+  display: block;
+  background-color: ${props.theme.lightAccent};
+  color: ${props.theme.baseColor};
+  padding: 0.2rem 1.3rem;
+  border-radius: 20px;
+  text-decoration: none;
+`;
+
+const AnchorTag = styled.a`
+  ${ButtonStyles};
+`;
+
+const LinkTag = styled(Link)`
+  ${ButtonStyles};
+`;
+
+const BasicButton = styled.button`
+  ${ButtonStyles};
+`;
+
+const Button = props => {
+  if (props.href) {
+    return (
+      <AnchorTag href={props.href} target="_blank">
+        {props.children}
+      </AnchorTag>
+    );
+  }
+
+  if (props.to) {
+    return <LinkTag to={props.to}>{props.children}</LinkTag>;
+  }
+
+  return <BasicButton type={props.type}>{props.children}</BasicButton>;
+};
+
+Button.defaultProps = {
+  type: 'button'
+};
+
+Button.propTypes = {
+  href: PropTypes.string,
+  to: PropTypes.string,
+  type: PropTypes.oneOf(['button', 'submit'])
+};
+
+export default Button;
