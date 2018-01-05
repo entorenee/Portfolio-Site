9482b44bc7d59dc191c4dc6fb8b412dde648c58b
Daniel Lemay <d.s.lemay@icloud.com>
2018-01-05 13:33:42 -0800

    Add Eslint

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  parser: 'babel-eslint',
+  env: {
+    browser: true,
+    es6: true
+  },
+  settings: {
+    ecmascript: 6,
+    jsx: true
+  },
+  parserOptions: {
+    ecmaVersion: 2017,
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+      experimentalDecorators: true,
+      jsx: true
+    },
+    sourceType: 'module'
+  },
+  plugins: ['react'],
+  extends: 'airbnb',
+  rules: {
+    'react/jsx-filename-extension': 0,
+    'comma-dangle': 0
+  }
+};
