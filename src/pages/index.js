00d8d2e90eaefa465d6969199f93fe6a1788f736
Daniel Lemay <d.s.lemay@icloud.com>
2018-01-16 21:25:21 -0800

    Add Contact section to index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Link from 'gatsby-link';
 import About from '../components/about/About';
 import Work from '../components/work/Work';
+import Contact from '../components/Contact';
 
 const { Fragment } = React;
 
@@ -9,6 +9,7 @@ const IndexPage = () => (
   <Fragment>
     <About />
     <Work />
+    <Contact />
   </Fragment>
 );
 
