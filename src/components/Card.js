916316004d5e4b702b5c438951d2cafe2495e3c6
Daniel Lemay <d.s.lemay@icloud.com>
2018-01-17 18:03:52 -0800

    Add further extensability to the Card component
    Several changes were made to the component to make it more reusable.
    
    * The centering in the parent element was removed so that it must be
    explicitly declared.
    * A default value of an empty string was added for the maxWidth prop.
    This allowed simplification of logic and adherance to best practice.
    * A style prop is now accepted which allows the user to pass an object
    of CSS additions or overwrites to the Card wrapper. It defaults to an
    empty object.

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,20 +3,29 @@ import PropTypes from 'prop-types';
 import styled from 'react-emotion';
 
 const CardWrapper = styled.div`
-  max-width: ${props => (props.maxWidth ? props.maxWidth : '')};
-  margin: 0 auto;
+  max-width: ${props => props.maxWidth};
   margin-bottom: 1.5rem;
   border: 1px solid #ccc
   padding: 0.5rem 0.25rem;
   box-shadow: 5px 5px 5px ${props => props.theme.darkAccent};
+  ${props => props.style};
 `;
 
 const Card = props => (
-  <CardWrapper maxWidth={props.maxWidth}>{props.children}</CardWrapper>
+  <CardWrapper style={props.style} maxWidth={props.maxWidth}>
+    {props.children}
+  </CardWrapper>
 );
 
 Card.propTypes = {
-  maxWidth: PropTypes.string
+  maxWidth: PropTypes.string,
+  style: PropTypes.object, // eslint-disable-line react/forbid-prop-types
+  children: PropTypes.node.isRequired
+};
+
+Card.defaultProps = {
+  maxWidth: '',
+  style: {}
 };
 
 export default Card;
