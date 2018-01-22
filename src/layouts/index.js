c8cba93e91274747ae808b578f709b048811eb96
Daniel Lemay <d.s.lemay@icloud.com>
2018-01-22 08:19:42 -0800

    Add global h1 color style

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,10 +3,18 @@ import PropTypes from 'prop-types';
 import Link from 'gatsby-link';
 import Helmet from 'react-helmet';
 import { ThemeProvider } from 'emotion-theming';
+import { injectGlobal } from 'emotion';
 import Header from '../components/header/Header';
 
 // import './index.css';
 import theme from './emotionTheme';
+
+// eslint-disable-next-line no-unused-expressions
+injectGlobal`
+  h1 {
+    color: #2D3B7F;
+  }
+`;
 
 const TemplateWrapper = ({ children }) => (
   <ThemeProvider theme={theme}>
