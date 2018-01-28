796b9c1b6e88a77a525b74eadb3a12c6937e2135
Daniel Lemay <d.s.lemay@icloud.com>
2018-01-27 22:28:25 -0800

    Add Contentful source plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,22 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`
+    title: 'Gatsby Default Starter'
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-emotion`,
-    `gatsby-plugin-react-next`,
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-react-next',
     {
-      resolve: `gatsby-plugin-typography`,
+      resolve: 'gatsby-plugin-typography',
       options: {
         pathToConfigModule: 'src/utils/typography.js'
+      }
+    },
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: '024qyvhyq0tv',
+        accessToken: '4d263766bb6101d57642e0e5a3aa85f3f5d98a7c055f221fc4ff52be8aeb059f'
       }
     }
   ]
