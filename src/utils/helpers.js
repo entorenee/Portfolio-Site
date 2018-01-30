4306b93507028ba83253a0dd279bcc6b3fb2c047
Daniel Lemay <d.s.lemay@icloud.com>
2018-01-29 22:04:40 -0800

    Create blog post slug function

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,2 +1,12 @@
+const slugify = require('slugify');
+
 // Return if enter or spacebar was pressed for accessible keyboard handling
 exports.keyboardHandler = e => e.which === 13 || e.which === 32;
+
+exports.postSlug = (date, title) => {
+  const titleSlug = slugify(title, {
+    remove: /[^A-Za-z0-9\s]+/,
+    lower: true
+  });
+  return `blog/${date}/${titleSlug}`;
+};
