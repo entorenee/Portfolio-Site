7f49785cf191f0571ae58e1749dc31353486d2eb
Daniel Lemay <d.s.lemay@icloud.com>
2018-01-29 21:38:59 -0800

    Create pages for blog posts

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,47 @@
+const slugify = require('slugify');
+const path = require('path');
+
+exports.createPages = ({ graphql, boundActionCreators }) => {
+  const { createPage } = boundActionCreators;
+  return new Promise((resolve, reject) => {
+    const blogPostTemplate = path.resolve('src/templates/BlogPost.js');
+    resolve(
+      graphql(`
+        {
+          allContentfulBlogPost(limit: 100) {
+            edges {
+              node {
+                title
+                postDate(formatString: "YYYY/MM/DD")
+                id
+              }
+            }
+          }
+        }
+      `).then(result => {
+        if (result.errors) {
+          reject(result.errors);
+        }
+
+        result.data.allContentfulBlogPost.edges.forEach(edge => {
+          const titleSlug = slugify(edge.node.title, {
+            remove: /[^A-Za-z0-9\s]+/,
+            lower: true
+          });
+          const slug = `blog/${edge.node.postDate}/${titleSlug}`;
+          createPage({
+            path: slug,
+            component: blogPostTemplate,
+            context: {
+              id: edge.node.id
+            }
+          }); // end createPage
+        }); // end forEach
+      }) // end then
+    ); // end resolve
+  });
+};
+
 exports.onCreatePage = async ({ page, boundActionCreators }) => {
   const { createPage } = boundActionCreators;
   const pageExport = page;
