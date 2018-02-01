0ab3643d94858ad6691edf28ee406e1d7bcd33c2
Daniel Lemay <d.s.lemay@icloud.com>
2018-01-31 22:03:04 -0800

    Convert to class Component and add window scrolling
    The component was converted to a class based component to add the
    didMount life hook. This looks for a hash in the url location and
    parses out the location of the window depending on the existence of
    a hash. It also uses parent refs to target elements in child components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import About from '../components/about/About';
 import Work from '../components/work/Work';
 import Contact from '../components/Contact';
@@ -6,13 +6,48 @@ import Footer from '../components/Footer';
 
 const { Fragment } = React;
 
-const IndexPage = () => (
-  <Fragment>
-    <About />
-    <Work />
-    <Contact />
-    <Footer />
-  </Fragment>
-);
+class IndexPage extends Component {
+  constructor() {
+    super();
+    this.scrollToHash = this.scrollToHash.bind(this);
+  }
+
+  componentDidMount() {
+    setTimeout(this.scrollToHash, 0);
+  }
+
+  scrollToHash() {
+    const hash = window.location.hash.substring(1);
+    if (hash) {
+      this[hash].scrollIntoView();
+      window.scrollBy(0, -80);
+    } else {
+      window.scrollTo(0, 0);
+    }
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <About
+          inputRef={el => {
+            this.about = el;
+          }}
+        />
+        <Work
+          inputRef={el => {
+            this.work = el;
+          }}
+        />
+        <Contact
+          inputRef={el => {
+            this.contact = el;
+          }}
+        />
+        <Footer />
+      </Fragment>
+    );
+  }
+}
 
 export default IndexPage;
