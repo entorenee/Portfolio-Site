3bf0c226ebab4832c23a5f761911fc144fbb1a6e
Daniel Lemay <d.s.lemay@icloud.com>
2018-02-02 21:35:25 -0800

    Add RSS feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,11 @@
+const { postSlug } = require('./src/utils/helpers');
+
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter'
+    title: 'Daniel Lemay Blog',
+    description:
+      'Daniel Lemay is a full stack JavaScript developer based in Portland, OR and concentrating on cutting edge technologies.',
+    siteUrl: 'http://dslemay.com'
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -18,6 +23,57 @@ module.exports = {
       options: {
         spaceId: '024qyvhyq0tv',
         accessToken: '4d263766bb6101d57642e0e5a3aa85f3f5d98a7c055f221fc4ff52be8aeb059f'
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-feed',
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrel
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allContentfulBlogPost } }) =>
+              allContentfulBlogPost.edges.map(edge => {
+                const { title, postDate } = edge;
+                const { html } = edge.body.childMarkdownRemark;
+                return Object.assign(
+                  {},
+                  {
+                    url: `${site.siteMetadata.siteUrl}/${postSlug(title, postDate)}`,
+                    guid: `${site.siteMetadata.siteUrl}/${postSlug(title, postDate)}`,
+                    custom_elements: [{ 'content-encoded': html }]
+                  }
+                );
+              }),
+            query: `
+              {
+                allContentfulBlogPost(limit: 500, sort: { fields: [postDate], order: DESC }) {
+                  edges {
+                    node {
+                      title
+                      postDate(formatString: "YYYY/MM/DD")
+                      body {
+                        childMarkdownRemark {
+                          html
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: '/rss.xml'
+          }
+        ]
       }
     }
   ]
