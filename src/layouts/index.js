74fd6b539e83a3e9052c1b65773457ec0be032db
Daniel Lemay <d.s.lemay@icloud.com>
2018-02-05 18:56:24 -0800

    Add favicon

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,26 +3,25 @@ import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 import Header from '../components/header/Header';
 import Footer from '../components/Footer';
+import favicon from './favicon-32x32.png';
 
 import './index.css';
 
 const TemplateWrapper = ({ children, history }) => (
   <div>
-    <Helmet
-      title="Daniel Lemay | Full Stack JavaScript Developer"
-      meta={[
-        {
-          name: 'description',
-          content:
-            'Daniel Lemay is a full stack JavaScript developer, based out of Portland Oregon, specializing in responsive web applications using cutting edge technologies.'
-        },
-        {
-          name: 'keywords',
-          content:
-            'javascript, web development, developer, Portland, Oregon, front-end, full stack, back-end, react'
-        }
-      ]}
-    />
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>Daniel Lemay | Full Stack JavaScript Developer</title>
+      <meta
+        name="description"
+        content="Daniel Lemay is a full stack JavaScript developer, based out of Portland Oregon, specializing in responsive web applications using cutting edge technologies." // eslint-disable-line max-len
+      />
+      <meta
+        name="keywords"
+        content="javascript, web development, developer, Portland, Oregon, front-end, full stack, back-end, react" // eslint-disable-line max-len
+      />
+      <link rel="icon" type="image/png" href={favicon} />
+    </Helmet>
     <Header path={history.location.pathname} />
     <div>{children()}</div>
     <Footer />
