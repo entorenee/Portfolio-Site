41dda71a0216f9b44714e92a35a5d0a0830bc138
Daniel Lemay <26767995+dslemay@users.noreply.github.com>
2018-08-05 09:15:22 -0700

    Update Pomodoro Timer project

--- src/projects/pomodoro-timer/Components/Counter.js
+++ src/projects/pomodoro-timer/Components/Counter.js
@@ -1,58 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import { keyboardHandler } from '../helpers';
 import '../style/Counter.css';
 
-class Counter extends Component {
-  shouldComponentUpdate(nextProps) {
-    if (this.props.timerTotal !== nextProps.timerTotal) {
-      return true;
-    }
-    return false;
-  }
+const Counter = ({ adjustTimers, name, timerTotal }) => {
+  const timerName = name === 'sessionTime' ? 'Session Length' : 'Break Length';
 
-  render() {
-    const { name, adjustTimers, timerTotal } = this.props;
-    let timerName;
-    if (name === 'sessionTime') {
-      timerName = 'Session Length';
-    }
-    if (name === 'breakTime') {
-      timerName = 'Break Length';
-    }
-
-    return (
-      <div id={`${name}-timer-controls`} className="timer-controls">
-        <p className="timer-control-name">{timerName}</p>
-        <div className="counter-controls">
-          <div
-            className="adjust-time"
-            onClick={() => adjustTimers(name, '-')}
-            onKeyPress={e => {
-              if (keyboardHandler(e)) adjustTimers(name, '-');
-            }}
-            role="button"
-            tabIndex={0}
-          >
-            -
-          </div>
-          <div className="timerTotal">{timerTotal}</div>
-          <div
-            className="adjust-time"
-            onClick={() => adjustTimers(name, '+')}
-            onKeyPress={e => {
-              if (keyboardHandler(e)) adjustTimers(name, '+');
-            }}
-            role="button"
-            tabIndex={0}
-          >
-            +
-          </div>
-        </div>
+  return (
+    <div id={`${name}-timer-controls`} className="timer-controls">
+      <p className="timer-control-name">{timerName}</p>
+      <div className="counter-controls">
+        <button type="button" className="adjust-time" onClick={() => adjustTimers(name, '-')}>
+          -
+        </button>
+        <div className="timerTotal">{timerTotal}</div>
+        <button type="button" className="adjust-time" onClick={() => adjustTimers(name, '+')}>
+          +
+        </button>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 Counter.propTypes = {
   name: PropTypes.string.isRequired,
