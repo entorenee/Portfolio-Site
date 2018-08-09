30a1c221ee4e12e45c4936318dbbdca3c1eb8e9f
Daniel Lemay <26767995+dslemay@users.noreply.github.com>
2018-08-09 07:18:10 -0700

    Convert to new Router in Gatsby v2

--- __tests__/NavigationLinks.test.js
+++ __tests__/NavigationLinks.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router';
+import { Router } from '@reach/router';
 import renderer from 'react-test-renderer';
 import NavigationLinks from '../src/components/header/NavigationLinks';
 
@@ -7,9 +7,9 @@ describe('NavigationControls', () => {
   it('renders properly', () => {
     const tree = renderer
       .create(
-        <MemoryRouter>
+        <Router>
           <NavigationLinks home />
-        </MemoryRouter>,
+        </Router>,
       )
       .toJSON();
 
