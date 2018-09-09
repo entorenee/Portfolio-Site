596679f32849c78dc4491fd954eb6def9f16a2f8
Daniel Lemay <26767995+dslemay@users.noreply.github.com>
2018-09-09 12:12:27 -0700

    Use date-fns for date formatting

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,9 +1,17 @@
 const slugify = require('slugify');
+const { format } = require('date-fns');
 
-exports.postSlug = (date, title) => {
+const postSlug = (date, title) => {
   const titleSlug = slugify(title, {
     remove: /[^A-Za-z0-9\s]+/,
     lower: true,
   });
   return `blog/${date}/${titleSlug}`;
+};
+
+const longDateFormat = date => format(date, 'MMMM D, YYYY');
+
+module.exports = {
+  longDateFormat,
+  postSlug,
 };
