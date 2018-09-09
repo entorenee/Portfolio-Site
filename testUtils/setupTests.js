8e23b6299a604bd7067f69c673f7a3e2ad005b8b
Daniel Lemay <26767995+dslemay@users.noreply.github.com>
2018-09-09 10:26:56 -0700

    Add and setup jest-dom

--- testUtils/setupTests.js
+++ testUtils/setupTests.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-underscore-dangle */
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import 'jest-dom/extend-expect'
 
 global.___loader = {
   enqueue: jest.fn(),
