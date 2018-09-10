1831916a42bee1cb1145ee637bcc83be5296e24c
Daniel Lemay <26767995+dslemay@users.noreply.github.com>
2018-09-09 20:10:25 -0700

    Update tests for project carousel

--- src/components/work/__tests__/work.test.js
+++ src/components/work/__tests__/work.test.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { cleanup, fireEvent, render } from 'react-testing-library';
 
 import Carousel from '../carousel';
+import projects from '../carousel/project-spotlight';
 import Work from '..';
 
 afterEach(cleanup);
 
 describe('<Carousel />', () => {
   it('Navigation controls work properly', () => {
-    const { getByLabelText, getByText } = render(<Carousel />);
+    const { getByLabelText, getByTestId, getByText, queryByText } = render(<Carousel />);
 
     // Test play pause buttons
     fireEvent.click(getByLabelText('pause'));
@@ -19,9 +20,19 @@ describe('<Carousel />', () => {
 
     // Test next and previous buttons
     fireEvent.click(getByLabelText('next'));
-    expect(getByText('Simon Game')).toBeTruthy();
+    expect(getByText(projects[1].title)).toBeTruthy();
     fireEvent.click(getByLabelText('previous'));
-    expect(getByText('Wanderful')).toBeTruthy();
+    expect(getByText(projects[0].title)).toBeTruthy();
+
+    // Test when not playing
+    fireEvent.click(getByLabelText('pause'));
+    fireEvent.click(getByLabelText('next'));
+    expect(getByText(projects[1].title)).toBeTruthy();
+
+    // Test clicking selector boxes
+    fireEvent.click(getByTestId('project3'));
+    expect(getByText(projects[3].title)).toBeTruthy();
+    expect(queryByText(projects[0].title)).toBeNull();
   });
 });
 
