da628ebbcc2cc5275050416d4cf8435c4ecdd523
Daniel Lemay <26767995+dslemay@users.noreply.github.com>
2019-01-29 21:02:12 -0800

    Update Jest and related dependencies

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   verbose: true,
   transform: { '^.+\\.js$': 'babel-jest' },
   transformIgnorePatterns: ['node_modules/(?!(gatsby)/)'],
-  setupTestFrameworkScriptFile: '<rootDir>/testUtils/setupTests.js',
+  setupFilesAfterEnv: ['<rootDir>/testUtils/setupTests.js'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/testUtils/assetsTransformer.js',
