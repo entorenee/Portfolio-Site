3ec7fc3e3350f71d7365ded4de739f6a6ffb3290
Daniel Lemay <26767995+dslemay@users.noreply.github.com>
2019-02-15 11:17:50 -0800

    Add babel-preset-css-prop for tests

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,13 @@
 module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-react', '@babel/flow'],
-  plugins: ['@babel/plugin-proposal-class-properties', '@babel/plugin-syntax-dynamic-import'],
+  presets: [
+    '@babel/preset-env',
+    '@babel/preset-react',
+    '@babel/flow',
+    '@emotion/babel-preset-css-prop',
+  ],
+  plugins: [
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-syntax-dynamic-import',
+    'emotion',
+  ],
 };
