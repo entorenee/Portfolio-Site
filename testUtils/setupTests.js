1797b2b0a9387d4ee19357f61fca5dca534401b1
Daniel Lemay <26767995+dslemay@users.noreply.github.com>
2019-09-02 12:19:57 -0700

    test: remove unnecessary cleanup-after-each import

--- testUtils/setupTests.js
+++ testUtils/setupTests.js
@@ -1,6 +1,5 @@
 /* eslint-disable import/no-extraneous-dependencies, no-underscore-dangle */
 import '@testing-library/jest-dom/extend-expect';
-import '@testing-library/react/cleanup-after-each';
 
 global.___loader = {
   enqueue: jest.fn(),
