bfc10a3a780497b1378aa0451db17e989f21c676
Daniel Lemay <26767995+dslemay@users.noreply.github.com>
2019-10-06 09:19:27 -0700

    refactor: postDateSlug to replace hyphens

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2,11 +2,12 @@ const slugify = require('slugify')
 const { format } = require('date-fns')
 
 const postSlug = (date, title) => {
+  const urlDate = date.replace(/-/g, '/')
   const titleSlug = slugify(title, {
     remove: /[^A-Za-z0-9\s]+/,
     lower: true,
   })
-  return `blog/${date}/${titleSlug}`
+  return `/blog/${urlDate}/${titleSlug}`
 }
 
 const longDateFormat = date => format(date, 'MMMM D, YYYY')
