43cd36ae52701495fa35fb844171e8e8205ecba7
Skyler Lemay <26767995+dslemay@users.noreply.github.com>
2022-05-11 17:24:40 -0700

    chore: resolve eslint parser issue

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   plugins: ['@emotion'],
   extends: ['dslemay', 'dslemay/jest', 'dslemay/react', 'dslemay/jsx-a11y'],
+  parser: '@babel/eslint-parser',
   rules: {
     '@emotion/no-vanilla': 'error',
     '@emotion/import-from-emotion': 'error',
